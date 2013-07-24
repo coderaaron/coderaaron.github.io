bde423d287cd962702409d657b7908e0e0c40b5a
Aaron Graham <aegraham@gmail.com>
2013-07-23 21:41:05 -0500

    Google Slides import

---
+++ slide_config.js
@@ -0,0 +1,43 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'WUSTL Web Group',
+    subtitle: 'HTML5 and CSS3',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Aaron Graham',
+    company: 'Web Developer, MPA',
+    gplus: 'http://plus.google.com/116754823174491691761',
+    twitter: '@coderaaron',
+    github: 'http://github.com/coderaaron'
+  }, {
+    name: 'Brian Marston',
+    company: 'Web Developer, MPA',
+    gplus: 'http://plus.google.com/106570290873071944913',
+    twitter: '@b_marston',
+    www: 'http://www.fatdays.com'
+  }, {
+    name: 'Joe McGill Name',
+    company: 'Web Graphic Designer, DCM',
+    gplus: 'http://plus.google.com/105503316962559527271',
+    twitter: '@joemcgill',
+    www: 'http://joemcgill.net/'
+  }]
+};
+
